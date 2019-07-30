7089429fc2693f99563a2b8b4bd3859aff678fe9
Will (Yue) Du <982732+duwill@users.noreply.github.com>
2019-07-30 11:39:40 +0200

    Update copy-docker.sh

--- cicd/copy-docker.sh
+++ cicd/copy-docker.sh
@@ -4,6 +4,8 @@
 SRC="gcr.io/engineering-devops"
 #DEST="gcr.io/forgerock-io"
 DEST="willdu-forgerock.jfrog.io"
+DEST_USERNAME="admin"
+DEST_APIKEY="AKCp5dKiN2xr9LqXHea7AXFK5WvznfZ1Np3ZrR489To4Y5j1bf3xbww3f9p2jMguHzCxEwK9E"
 
 TAG="6.5.1"
 
@@ -16,6 +18,6 @@ IMAGES="openam ds openidm openig amster util git java"
 for image in $IMAGES; do 
     docker pull $SRC/$image:$TAG 
     docker tag $SRC/$image:$TAG $DEST/$image:$TAG 
-    docker login $DEST
+    docker login -u $DEST_USERNAME -p $DEST_APIKEY $DEST
     docker push $DEST/$image:$TAG 
 done
